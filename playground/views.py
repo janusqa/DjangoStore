5efb5356f2d4fc6fc189019e8df983543e5f33ad
Janus Developer <dev@retrievo.net>
2024-06-21 17:16:30 -0400

    use filtering to display products on hello page

--- playground/views.py
+++ playground/views.py
@@ -1,7 +1,39 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.core.exceptions import ObjectDoesNotExist
+from store.models import Product
 
 
 # Create your views here.
 def say_hello(request):
-    return render(request, "playground/hello.html", {"name": "JanusQA"})
+    query_set = Product.objects.all()
+
+    try:
+        product = Product.objects.get(pk=4)  # throws exception
+    except ObjectDoesNotExist:
+        pass
+
+    product = Product.objects.filter(
+        pk=4
+    ).first()  # does not throw exception if product not found
+
+    product_exists = Product.objects.filter(pk=4).exists()
+
+    query_set = Product.objects.filter(unit_price__gt=20)
+
+    query_set = Product.objects.filter(unit_price__range=(20, 30))
+
+    query_set = Product.objects.filter(
+        collection__id__range=(1, 3)
+    )  # span over attributes of foreign key
+
+    query_set = Product.objects.filter(title__contains="coffee")
+
+    query_set = Product.objects.filter(last_update__year=2021)  # extract parts of data
+
+    query_set = Product.objects.filter(description__isnull=True)
+
+    # for product in query_set:
+    #     print(product)
+
+    return render(request, "playground/hello.html", {"products": list(query_set)})
