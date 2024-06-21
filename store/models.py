151ec0f8863f097ae8feafb9c191ea359b306e13
Janus Developer <dev@retrievo.net>
2024-06-21 14:23:09 -0400

    scaffold the modles for the store app

--- store/models.py
+++ store/models.py
@@ -1,3 +1,97 @@
 from django.db import models
 
+
 # Create your models here.
+class Customer(models.Model):
+    MEMBERSHIP_BRONZE = "B"
+    MEMBERSHIP_SILVER = "S"
+    MEMBERSHIP_GOLD = "G"
+
+    MEMBERSHIP_CHOICES = [
+        (MEMBERSHIP_BRONZE, "Bronze"),
+        (MEMBERSHIP_SILVER, "Silver"),
+        (MEMBERSHIP_GOLD, "Gold"),
+    ]
+
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    email = models.EmailField(unique=True)
+    phone = models.CharField(max_length=255)
+    birth_date = models.DateField(null=True)
+    membership = models.CharField(
+        max_length=1, choices=MEMBERSHIP_CHOICES, default=MEMBERSHIP_BRONZE
+    )
+
+
+class Address(models.Model):
+    street = models.CharField(max_length=255)
+    city = models.CharField(max_length=255)
+    # customer = models.OneToOneField(
+    #     Customer, on_delete=models.CASCADE, primary_key=True
+    # ) # one-to-one relationship
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)  # one-to-many
+
+
+class Promotion(models.Model):
+    description = models.CharField(max_length=255)
+    discount = models.FloatField()
+
+
+# 1. Name clashes: use related_name='+' to tell django we want to buy pass creating the reverse
+# relationship, as it already exists from previous work. This will prevent
+# name clashes. You can also use realated_name to give relation a diffent name too
+# to avoid clashing with an existing name.
+# 2. Circular dependancy where Collection has a relationship with Product and
+# Product has relationship with Collection. We solve this by putting for instanmce
+# Proudct in quotations. That resolves the dependance where product has to be
+# declared before Collection.
+class Collection(models.Model):
+    title = models.CharField(max_length=255)
+    featured_product = models.ForeignKey(
+        "Product", on_delete=models.SET_NULL, null=True, related_name="+"
+    )
+
+
+class Product(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    inventory = models.IntegerField
+    last_update = models.DateTimeField(auto_now=True)
+    collection = models.ForeignKey(Collection, on_delete=models.PROTECT)
+    promotions = models.ManyToManyField(Promotion)
+
+
+class Cart(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+class CartItem(models.Model):
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveSmallIntegerField()
+
+
+class Order(models.Model):
+    PAYMENT_STATUS_PENDING = "P"
+    PAYMENT_STATUS_COMPLETE = "C"
+    PAYMENT_STATUS_FAILED = "F"
+
+    PAYMENT_STATUS_CHOICES = [
+        (PAYMENT_STATUS_PENDING, "Pending"),
+        (PAYMENT_STATUS_COMPLETE, "Complete"),
+        (PAYMENT_STATUS_FAILED, "Failed"),
+    ]
+
+    customer = models.ForeignKey(Customer, on_delete=models.PROTECT)
+    payment_status = models.CharField(
+        max_length=1, choices=PAYMENT_STATUS_CHOICES, default=PAYMENT_STATUS_PENDING
+    )
+    placed_at = models.DateTimeField(auto_now_add=True)
+
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.PROTECT)
+    product = models.ForeignKey(Product, on_delete=models.PROTECT)
+    quantity = models.PositiveSmallIntegerField()
+    unit_price = models.DecimalField(max_digits=6, decimal_places=2)
