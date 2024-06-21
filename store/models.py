9249883c0b5ade259030ac28dc5f17842757ec15
Janus Developer <dev@retrievo.net>
2024-06-21 15:25:51 -0400

    revert a migration

--- store/models.py
+++ store/models.py
@@ -22,11 +22,6 @@ class Customer(models.Model):
         max_length=1, choices=MEMBERSHIP_CHOICES, default=MEMBERSHIP_BRONZE
     )
 
-    class Meta:
-        indexes = [
-            models.Index(fields=["last_name", "first_name"]),
-        ]
-
 
 class Address(models.Model):
     street = models.CharField(max_length=255)
